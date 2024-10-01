15c56729e1b16ed3c936d622a4a96096c33202c5
ezequiel_pc <estebanpalacios170@gmail.com>
2024-10-01 11:51:31 -0300

    actividadAnexo2.js

--- actividadAnexo2.js
+++ actividadAnexo2.js
@@ -1,11 +1,9 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const CopyPairs = (arr) => {
-    
-    const pares = arr.filter(num => num % 2 === 0);
-    return pares;
-}
+    return arr.map(num => (num % 2 === 0 ? num : null)).filter(num => num !== null);
+  };
+  
 
-const paresResultado = CopyPairs(numeros);
-
-paresResultado.forEach(par => console.log(par));
+  console.log(CopyPairs([2, 3, 5, 12, 31])); 
+  console.log(CopyPairs([10, 18, 191, 13, 248])); 
+  
